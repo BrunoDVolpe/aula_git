49c8d71c91afb99c512d074fa3921e6b93eb21ab
Bruno <bdva16@hotmail.com>
2024-04-28 11:58:58 -0300

    Feat: Add encryption to Aluno create before save; add compare password hash at LoginController

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize')
 const { connection } = require("../database/connection");
+const { hash } = require("bcryptjs")
 
 // define é o método para conectar com uma tabela específica. Passa a tabela e as colunas que quero conectar.
 // Meu DataTypes não veio sozinho, precisei importar manualmente.
@@ -19,6 +20,11 @@ const Aluno = connection.define('alunos', {
     celular: {
         type: DataTypes.STRING
     }
+})
+
+// Hooks (gatilhos) Sequelize - Ações que o próprio Sequelize permite usarmos.
+Aluno.beforeSave(async (aluno) => {
+    aluno.password = await hash(aluno.password, 8)
 })
 
 module.exports = Aluno
