5cfc6501d81b0aaa4bba3cda4488cd7738d4f26b
Bruno <bdva16@hotmail.com>
2024-04-29 08:13:47 -0300

    Feat: add create route to CursoController

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -16,6 +16,28 @@ class CursoController {
 
         res.json(cursos)
     }
+
+    async create(req, res) {
+        try {
+            const { nome, duracao_horas } = req.body
+            if(!nome) {
+                return res.status(400).json({message: "O nome é obrigatório"})
+            }
+            if(!(duracao_horas >= 40 && duracao_horas <= 200)) {
+                return res.status(400).json({message: "A duração do curso deve ser entre 40 e 200 horas"})
+            }
+            const curso = await Curso.create({
+                nome: nome,
+                duracao_horas: duracao_horas
+            })
+            
+            res.status(201).json(curso)
+    
+        } catch(err) {
+            console.log(err.message)
+            res.status(500).json({error: 'Erro ao criar o curso.'})
+        }
+    }
 }
 
 module.exports = new CursoController()
